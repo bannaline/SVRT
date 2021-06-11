6ad07d9df95c53664a29b424c1a972100c793cd7
bannaline <45658921+bannaline@users.noreply.github.com>
2021-06-11 18:47:44 +0900

    alpha v1
    대략적인 완성

--- data_manage.py
+++ data_manage.py
@@ -10,7 +10,7 @@ from datetime import datetime
 def load_data(self):
     conn = sqlite3.connect('log.db')
     cursor = conn.cursor()
-    result = cursor.execute("SELECT * FROM log ORDER BY LogTime DESC LIMIT 10")
+    result = cursor.execute("SELECT Date, CardPack, Mod, MyJob, MyArche, OppoJob, OppoArche, FirstSecond, WinLose, Turn FROM log ORDER BY LogTime DESC LIMIT 10")
     rows = result.fetchall()
     row_cnt_query = cursor.execute("SELECT * FROM log")
     row_cnt = row_cnt_query.fetchall()
@@ -19,12 +19,66 @@ def load_data(self):
         self.tableRecord.setRowCount(len(row_cnt))
     for i, row in enumerate(rows):
         for j, data in enumerate(row):
+            if i == 0:
+                last_record(self, j, data)
             item = QTableWidgetItem()
             item.setText(str(data))
             item.setTextAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignHCenter)
             self.tableRecord.setItem(i, j, item)
     conn.close()
     self.statusBar().showMessage(sb_msg)
+
+
+def last_record(self, j, data):
+    data = str(data)
+    if j == 2:
+        if data == "로테이션":
+            self.radioRecRota.setChecked(True)
+        elif data == "언리미티드":
+            self.radioRecUnli.setChecked(True)
+        self.rad_mod()
+
+    if j == 3:
+        mycls = ['로얄', '위치', '엘프', '비숍', '드래곤', '네크로맨서', '뱀파이어', '네메시스']
+        mycls_btn = [self.radioRoyalMy, self.radioWitchMy, self.radioElfMy, self.radioBishopMy,
+                     self.radioDragonMy, self.radioNecroMy, self.radioVampMy, self.radioNemeMy]
+        num = mycls.index(data)
+        mycls_btn[num].setChecked(True)
+        self.radio_myjob()
+
+    if j == 4:
+        self.comboArcheMy.setCurrentText(data)
+        self.cb_my_arche()
+
+    if j == 5:
+        oppocls = ['로얄', '위치', '엘프', '비숍', '드래곤', '네크로맨서', '뱀파이어', '네메시스']
+        oppocls_btn = [self.radioRoyalOppo, self.radioWitchOppo, self.radioElfOppo, self.radioBishopOppo,
+                       self.radioDragonOppo, self.radioNecroOppo, self.radioVampOppo, self.radioNemeOppo]
+        num = oppocls.index(data)
+        oppocls_btn[num].setChecked(True)
+        self.radio_oppojob()
+
+    if j == 6:
+        self.comboArcheOppo.setCurrentText(data)
+        self.cb_oppo_arche()
+
+    if j == 7:
+        if data == '선공':
+            self.radioFirst.setChecked(True)
+        elif data == '후공':
+            self.radioSecond.setChecked(True)
+        self.rad_fs()
+
+    if j == 8:
+        if data == '승':
+            self.radioWin.setChecked(True)
+        elif data == '패':
+            self.radioLose.setChecked(True)
+        self.rad_wl()
+
+    if j == 9:
+        self.comboTurn.setCurrentText(data)
+        self.cb_turn()
 
 
 def write_record(self, a, b, c, d, e, f, g, h, i, j):
